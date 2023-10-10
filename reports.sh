54501988867589c4afe91b3c779b98d4279a3e3a
Ryan Kitty <ryan@kittydiaz.com>
2023-10-10 13:59:04 -0500

    initial commit
    Easy way to see player reports and bans

---
+++ reports.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+clear
+echo Running gamename Reports...
+sleep 3
+echo 
+echo gamename Player Reports Below:
+echo
+cat /path/to/server/addons/sourcemod/configs/playerreport_logs.txt
+echo gamename Ban Reports Below:
+echo 
+cat /path/to/server/addons/sourcemod/logs/lilac.log
+echo -------
+echo
+echo End of reports.
