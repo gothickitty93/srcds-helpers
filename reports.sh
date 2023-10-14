aef6667f4383aad05cea3f79ae4959f616551586
Ryan Kitty <ryan@kittydiaz.com>
2023-10-14 12:03:22 -0500

    added additional log paths

--- reports.sh
+++ reports.sh
@@ -6,9 +6,13 @@ echo
 echo gamename Player Reports Below:
 echo
 cat /path/to/server/addons/sourcemod/configs/playerreport_logs.txt
+echo 
+cat /path/to/server/addons/sourcemod/logs/vote_kick.log
 echo gamename Ban Reports Below:
 echo 
 cat /path/to/server/addons/sourcemod/logs/lilac.log
+echo
+cat /path/to/server/addons/sourcemod/logs/SMAC.log
 echo -------
 echo
 echo End of reports.
