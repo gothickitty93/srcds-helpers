c2d6a9dcc35089ddadd6bcc7ca0ed46c8d3bdfb4
Ryan Kitty <ryan@kittydiaz.com>
2023-12-26 16:45:18 -0600

    uploaded reportclear script
    This is the script I use on my L4D2 server, but it can easily be changed to remove whatever files from whatever path you want.

---
+++ l4d2_reportclear.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+clear
+echo Clearing L4D2 Reports...
+sleep 3
+echo 
+echo Clearing L4D2 Player Reports...
+echo
+rm /root/composers/linuxgsm/L4D2ds/serverfiles/left4dead2/addons/sourcemod/configs/playerreport_logs.txt
+echo 
+rm /root/composers/linuxgsm/L4D2ds/serverfiles/left4dead2/addons/sourcemod/logs/vote_kick.log
+echo Clearing L4D2 Ban Reports...
+echo 
+rm /root/composers/linuxgsm/L4D2ds/serverfiles/left4dead2/addons/sourcemod/logs/lilac.log
+echo
+rm /root/composers/linuxgsm/L4D2ds/serverfiles/left4dead2/addons/sourcemod/logs/SMAC.log
+echo -------
+echo
+echo Clear complete.
